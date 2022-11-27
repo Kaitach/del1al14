e3c030e1b231e0186c001480b089c6b6e1cb4c23
Kaitach <104739161+Kaitach@users.noreply.github.com>
2022-11-27 19:35:15 -0300

    Programa 9 de 10

--- progrma9de14/src/Main.java
+++ progrma9de14/src/Main.java
@@ -3,31 +3,39 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
-
         int cantidad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de numeros:"));
         int numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el primer numero:"));
         int numero2 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el segundo numero:"));
+        int numero3 = 800000000 ;
 
-
-        for (int i=0; i<cantidad; i++){
+        for (int i=0; i<(cantidad); i++){
             if (numero > numero2) {
-                JOptionPane.showMessageDialog(null , " " + numero + " es mayor que " + numero2 );
+                if (numero3 > numero2) {
+                    numero3 = numero2;
+                }
                 numero2 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el nuevo numero:"));
             }
             else {
-                JOptionPane.showMessageDialog(null , " " + numero2 + " es mayor que " + numero );
+                if (numero3 >  numero) {
+                    numero3 = numero;
+                }
                 numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el nuevo numero:"));
-
             }
-
 
         }
         if (numero > numero2) {
-            JOptionPane.showMessageDialog(null , " " + numero + " es mayor que " + numero2 );
-        }
-        else {
-            JOptionPane.showMessageDialog(null , " " + numero2 + " es mayor que " + numero );
+            if (numero3 > numero2) {
+                numero3 = numero2;
+            }
 
         }
+        else {
+            if (numero3 >  numero) {
+                numero3 = numero;
+            }
+
+        }
+            JOptionPane.showMessageDialog(null , " De los numeros ingresados el mayor es:  " + numero + " y el menor:  " + numero3 );
+        }
+
     }
-}
